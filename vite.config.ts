226c911a4a6bed46263ddcdcc642422c206946a1
root <root@localhost.localdomain>
2026-06-20 17:03:05 +0800

    prepare

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/Motorola68000_Simulator/',
   plugins: [vue()],
   resolve: {
     alias: {
